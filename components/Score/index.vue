6aa127ca2f3125e6110b5a9117d57f4809cb5ef0
leochocolat <l.mouraire@outlook.fr>
2023-11-16 17:16:28 +0100

    :sparkles: Leaderboard > Add scroe valueString key

--- components/Score/index.vue
+++ components/Score/index.vue
@@ -20,7 +20,7 @@
         <div class="col-right">
 
             <div ref="value" class="value">
-                {{ data.value }}
+                {{ data.valueString || data.value }}
             </div>
 
             <div ref="date" class="date">
